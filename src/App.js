207c9a4971c8b37bffe8a449dbc9a848a6a7af7b
devwiliantavora <angeltavora@gmail.com>
2022-04-21 21:43:09 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import react from 'react'
+import {FiSearch} from 'react-icons/fi'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Buscador CEP</h1>
+      <input
+      type='text'
+      placeholder='Digite seu CEP'
+      ></input>
+      <button><FiSearch /></button>
+
+
+
     </div>
   );
 }
